53ba165b6b9b82c0f723857daf4c604198e8c810
季苏媛 <jisuyuan@jisuyuandeMacBook-Pro.local>
2015-10-09 13:04:32 -0400

    abc

--- script/script.js
+++ script/script.js
@@ -11,7 +11,7 @@ var plot = d3.select('.canvas')
 	.append('g')
 	.attr('class','plot')
 	//.attr('transform','translate()') --> complete this line here
-
+	.attr('transform','translate('+margin.l+','+margin.t+')');
 
 //Start with 100 symbols
 var numOfSymbols = 100;
@@ -23,9 +23,50 @@ var numOfSymbols = 100;
 // --> size: between 0 and 100x100
 // --> type: circle or square
 // --> color
-var symbols = [];
 
 //With the array you've created and populated, draw circles to represent these symbols
-symbols.forEach(function(symbol){
-	
-})
+var Symbols = [];
+function runRandomSymbols() {
+
+
+	for (var i = 0; i < numOfSymbols; i++) {
+		console.log("rgb"+"("+Math.round((Math.random())*255)+", 0, 0)");
+		var object = {
+			x: Math.random() * width,
+			y: Math.random() * height,
+			size: [Math.random() * 50, Math.random()*50],
+			type: Math.round(Math.random()),
+			color: Math.round((Math.random())*255)
+		}
+		Symbols.push(object);
+	}
+	console.log(object);
+	console.log(Symbols);
+}
+runRandomSymbols();
+
+Symbols.forEach(drawdraw);
+
+function drawdraw(symbol) {
+	//decide drawn what shape
+	if (symbol.type == 1) {
+		plot
+			.append('circle')
+			.attr('cx', symbol.x)
+			.attr('cy', symbol.y)
+			.attr('r', symbol.size[0])
+			.style('fill', "rgb"+"("+symbol.color+", 0, 0)")
+
+	} else {
+		plot
+			.append('rect')
+			.attr('x', symbol.x)
+			.attr('y', symbol.y)
+			.attr('width', symbol.size[0])
+			.attr('height', symbol.size[1])
+			.style('fill', symbol.color)
+	}
+
+
+}
+
